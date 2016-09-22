250d3ce55374108a47721900a215df6cb885928a
Larry Hernandez <larryhernanjr@gmail.com>
2016-09-22 13:55:45 -0400

    add login functionality and user details storage in firebase

---
+++ src/components/register.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import firebase from '../../firebase.config.js';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(e) {
+    const stateObj = {};
+    const stateKey = e.target.name;
+    stateObj[stateKey] = e.target.value;
+    this.setState(stateObj);
+  }
+  handleSubmit() {
+    const { email, password } = this.state;
+    firebase.auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch((err) => {
+        console.log(err);
+      })
+      .then((user) => {
+        firebase.database().ref('users')
+          .child(user.uid)
+          .set({ first_name: '', last_name: '', e_mail: email });
+      })
+      .then(() => {
+        this.props.router.push('/dashboard');
+      });
+  }
+  render() {
+    return (
+      <div>
+        <div id="register-form">
+          <div>
+            <input name="email" onChange={this.handleChange} type="text" placeholder="email" />
+          </div>
+          <div>
+            <input name="password" onChange={this.handleChange} type="text" placeholder="password" />
+          </div>
+          <button className="button" onClick={this.handleSubmit}>Register</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Register);
--- src/config/routes.jsx
+++ src/config/routes.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Router, IndexRoute, Route, hashHistory } from 'react-router';
 import Main from '../components/main.jsx';
 import Home from '../components/home.jsx';
+import Register from '../components/register.jsx';
 
 const Routes = () => {
   return (
     <Router history={hashHistory}>
       <Route path="/" component={Main}>
         <IndexRoute component={Home} />
+        <Route path="register" component={Register} />
       </Route>
     </Router>
   );
